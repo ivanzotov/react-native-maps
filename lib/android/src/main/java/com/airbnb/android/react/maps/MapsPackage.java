c241d281e09d1060491e51b5cb35e9db14f39600
Ivan Zotov <ivanzotov@gmail.com>
2017-08-08 16:40:45 +0300

    Fix for RN 47

--- lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/MapsPackage.java
@@ -3,7 +3,6 @@ package com.airbnb.android.react.maps;
 import android.app.Activity;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -22,11 +21,6 @@ public class MapsPackage implements ReactPackage {
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     return Arrays.<NativeModule>asList(new AirMapModule(reactContext));
-  }
-
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
   }
 
   @Override
